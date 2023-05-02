015126cb27ae861261bdd865b18b674a5de486d6
ajkn1904 <ajknishat@gmail.com>
2023-05-02 15:47:44 +0600

    counter in the home page added

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -2,6 +2,7 @@ import NavBar from "@/pages/NavBar";
 import Banner from "./Banner";
 import SearchTour from "./SearchTour";
 import AboutUs from "./AboutUs";
+import StatCounter from "./StatCounter";
 
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
             <Banner />
             <SearchTour />
             <AboutUs />
+            <StatCounter/>
         </>
     );
 };
---
+++ src/Components/StatCounter.tsx
@@ -0,0 +1,49 @@
+import { TiGroup } from 'react-icons/ti';
+import { MdOutlineTravelExplore } from 'react-icons/md';
+import { GiHiking } from 'react-icons/gi';
+import { RiHotelFill } from 'react-icons/ri';
+
+
+const StatCounter = () => {
+    return (
+        <div className='w-[85vw] mx-auto flex flex-col lg:flex-row justify-center items-center mt-40 mb-20'>
+
+            <div className='flex gap-12 h-[200px] md:h-[250px] lg:h-[200px] border-b lg:border-b-0 lg:border-r md:pr-5 md:gap-r-0 lg:pr-0'>
+                
+                
+                <div className='border-r w-[200px] md:w-[270px] h-[200px] md:h-[250px] px-3 py-5 md:p-12 lg:h-[200px] lg:w-[210px] lg:p-5'>
+                    <TiGroup className='h-24 w-24 rounded-full bg-green-200 border-8 border-green-100 p-5 text-green-700' />
+                    <h1 className='text-4xl font-semibold'>100,000+</h1>
+                    <p className='text-lg'>Counter One</p>
+                </div>
+
+                <div className='w-[170px] h-[200px] md:w-[200px] md:h-[300px] p-5 md:p-12 lg:h-[200px] lg:w-[210px] lg:p-5'>
+                    <MdOutlineTravelExplore className='h-24 w-24 rounded-full bg-green-200 border-8 border-green-100 p-5 text-green-700' />
+                    <h1 className='text-4xl font-semibold'>5,000+</h1>
+                    <p className='text-lg'>Destinations</p>
+
+                </div>
+            </div>
+
+
+
+
+            <div className='flex gap-12 md:pl-5 md:gap-l-0 h-[200px]'>
+                <div className='border-r w-[200px] md:w-[230px] h-[200px] md:h-[250px] px-3 py-5 md:px-4 md:py-12 lg:h-[200px] lg:w-[210px] lg:p-5'>
+                    <GiHiking className='h-24 w-24 rounded-full bg-green-200 border-8 border-green-100 p-5 text-green-700' />
+                    <h1 className='text-4xl font-semibold'>10,000+</h1>
+                    <p className='text-lg'>More Trips</p>
+                </div>
+
+                <div className='w-[170px] h-[200px] p-5 md:w-[200px] md:h-[300px] md:p-12 lg:h-[200px] lg:p-5 lg:w-[210px]'>
+                    <RiHotelFill className='h-24 w-24 rounded-full bg-green-200 border-8 border-green-100 p-5 text-green-700' />
+                    <h1 className='text-4xl font-semibold'>2,000+</h1>
+                    <p className='text-lg'>Luxary Hotel</p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default StatCounter;
