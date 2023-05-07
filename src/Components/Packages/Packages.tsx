ff5bda8fb872ad2289869755040adb078ca5036a
ajkn1904 <ajknishat@gmail.com>
2023-05-08 03:26:54 +0600

    updated

--- src/Components/Packages/Packages.tsx
+++ src/Components/Packages/Packages.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { AiFillStar } from 'react-icons/ai';
+import { BiTimeFive, BiDumbbell } from 'react-icons/bi';
+import initAOS from '../../../aos';
 
 const Packages = ({ data }: any) => {
-    console.log(data);
+
+    useEffect(() => {
+        initAOS();
+    }, []);
+
+    //console.log(data);
+
+    const previousPrice = data.price + 1700
+
+
     return (
-        <div key={data._id} className="card md:card-side bg-base-100 shadow-xl my-20">
-            <figure className=''><img src={data.thumbnail_image} className='h-[250px] sm:h-[300px] w-full md:w-[300px]' alt="Movie" /></figure>
-            <div className="card-body md:w-[900px]">
-                <h2 className="card-title">{data.name}</h2>
-                <p>{data.description}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn bg-green-600 text-white border-green-500 hover:border-2 hover:border-yellow-500 shadow-2xl shadow-blue-700">View Details</button>
+        <div key={data._id} className="card md:card-side bg-base-100 shadow-xl my-20" data-aos="fade-up" data-aos-offset="10" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-once="false">
+
+            <figure data-aos="zoom-in" data-aos-offset="10" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-once="false">
+                <img src={data.thumbnail_image} className='h-[250px] sm:h-[280px] w-full md:w-[300px]' alt="Movie" />
+            </figure>
+
+
+            <div className="card-body md:w-[900px] flex sm:flex-row" data-aos="fade-right" data-aos-offset="10" data-aos-delay="50" data-aos-duration="1100" data-aos-easing="ease-in-out" data-aos-once="false">
+
+
+                <div className='sm:w-[70%] sm:border-r sm:pr-3 sm:mr-3'>
+                    <h2 className="card-title">{data.name}</h2>
+                    <p>{data.description.slice(0, 120)}...</p>
+
+                    <div className='flex justify-start items-center gap-2 mt-8'>
+                        <div className='flex items-center gap-2 bg-gray-100 px-3 py-1'>
+                            <BiTimeFive className='text-green-600 h-5 w-5' />
+                            <p>{data.duration} days</p>
+                        </div>
+                        <div className='flex items-center gap-2 bg-gray-100 px-3 py-1'>
+                            <BiDumbbell className='text-green-600 h-5 w-5' />
+                            <p>{data.physicality}</p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div className="flex-col w-[150px] items-center md:mx-auto" data-aos="fade-left" data-aos-offset="10" data-aos-delay="50" data-aos-duration="1200" data-aos-easing="ease-in-out" data-aos-once="false">
+
+                    <div className="flex gap-1 mt-10 sm:mt-0 md:mt-1">
+                        <AiFillStar className="text-yellow-400 h-5 w-5" />
+                        <AiFillStar className="text-yellow-400 h-5 w-5" />
+                        <AiFillStar className="text-yellow-400 h-5 w-5" />
+                        <AiFillStar className="text-yellow-400 h-5 w-5" />
+                        <AiFillStar className="text-yellow-400 h-5 w-5" />
+                    </div>
+
+                    <p className='text-2xl font-semibold text-blue-400 my-2 md:mt-2 line-through'>$ {previousPrice}</p>
+
+                    <p className='text-4xl font-semibold text-blue-600 mt-2 md:mt-0 '>$ {data.price}</p>
+
+                    <p className='mb-5 text-gray-400'>per person</p>
+
+                    <button className="btn bg-green-600 text-white border-green-500 hover:border-2 hover:border-yellow-500 shadow-lg shadow-blue-300">View Details</button>
                 </div>
             </div>
         </div>
--- src/pages/Tours.tsx
+++ src/pages/Tours.tsx
@@ -14,7 +14,7 @@ const Tours = ({ tourPackages }: any) => {
             <h1 className="text-center text-2xl text-green-600 italic font-semibold my-10">All Packages_____</h1>
 
 
-            <div className='w-[85vw] mx-auto'>
+            <div className='w-[85vw] lg:w-[70vw] mx-auto max-w-[1200px]'>
                 {
                     tourPackages && tourPackages.map((pack: any) => <Packages key={pack._id} data={pack} />)
 
