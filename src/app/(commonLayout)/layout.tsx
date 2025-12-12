315b7a2031547961875323f3b52e3443701fe85e
ajkn1904 <ajknishat@gmail.com>
2025-12-12 10:04:52 +0600

    bug fixed

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,12 +1,15 @@
 import Footer from "@/src/components/shared/Footer/Footer";
 import NavBar from "@/src/components/shared/Navbar/Navbar";
+import { Suspense } from "react";
 
-const CommonLayout = ({ children } : { children: React.ReactNode }) => {
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <>  
-            <NavBar/>
-             <main className="min-h-[75vh]">{children}</main>
-            <Footer/>
+        <>
+            <NavBar />
+            <Suspense fallback={<p>Loading page...</p>}>
+                <main className="min-h-[75vh]">{children}</main>
+            </Suspense>
+            <Footer />
         </>
     );
 };
